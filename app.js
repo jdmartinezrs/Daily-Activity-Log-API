4835a51d0b899bdaeb39cd7e84eae032dd1b2aa1
jdmartinezrs <157431644+jdmartinezrs@users.noreply.github.com.>
2024-10-23 22:17:16 -0500

    feat: :construction: some modifications were done at this point im with mongodb instead mongoose

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
+const http = require('http');
 const fs = require('fs');
+
 const ConnectToDatabase = require('./infraestructure/database/mongodb');
 const createServer = require('./infraestructure/server/server');
-require('dotenv').config({ path: '../.env' });
 
 const startApp = async () => {
+    
     let connectToDatabase = new ConnectToDatabase();
-    await connectToDatabase.open();
+    await connectToDatabase.connectOpen();
 
     const app = createServer();
 
-    app.listen({ port: process.env.EXPRESS_PORT, host: process.env.EXPRESS_HOST }, () => {
+    const httpServer = http.createServer(app);
+
+    httpServer.listen({ port: process.env.EXPRESS_PORT, host: process.env.EXPRESS_HOST }, () => {
         console.log(`http://${process.env.EXPRESS_HOST}:${process.env.EXPRESS_PORT}`);
     });
 };
 
 startApp();
+
 
